868ebccc1a3740132785eb105c4409f5357edcfe
Andrew Smith <andrewchristophersmith@gmail.com>
2017-08-19 22:58:07 -0700

    init

---
+++ src/getConstructor/fromRequireContext.js
@@ -0,0 +1,29 @@
+// Load React components by requiring them from "components/", for example:
+//
+// - "pages/index" -> `require("components/pages/index")`
+// - "pages/show.Header" -> `require("components/pages/show").Header`
+// - "pages/show.Body.Content" -> `require("components/pages/show").Body.Content`
+//
+module.exports = function(reqctx) {
+  return function(className) {
+    var parts = className.split(".")
+    var filename = parts.shift()
+    var keys = parts
+    // Load the module:
+    var component;
+    try {
+      component = reqctx("./" + filename)
+    } catch(err) {
+      component = reqctx("./" + filename + '.jsx')
+    }
+    // Then access each key:
+    keys.forEach(function(k) {
+      component = component[k]
+    })
+    // support `export default`
+    if (component.__esModule) {
+      component = component["default"]
+    }
+    return component
+  }
+}
